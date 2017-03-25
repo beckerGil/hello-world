919e01246e58476487e2331c6f724b43d2fe6c74
Gil Becker <becker.gil@gmail.com>
2017-03-25 19:31:53 +0300

    button

--- GitPlay/app/src/main/java/com/gil_becker/gitplay/MainActivity.java
+++ GitPlay/app/src/main/java/com/gil_becker/gitplay/MainActivity.java
@@ -1,7 +1,15 @@
 package com.gil_becker.gitplay;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LinearLayout linearLayout = new LinearLayout(this);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        Button Btn = new Button(this);
+        linearLayout.addView(Btn,new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT));
+        Btn.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                Toast.makeText(v.getContext(),"wow",Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+        Btn.setGravity(Gravity.CENTER);
+        Btn.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+        setContentView(linearLayout);
     }
 }
